d7d179b1be3d9020a1643a2e9c77e762d19b8c1b
bin <bin@qq.com>
2025-11-01 14:57:30 +0800

    初步优化qconfig的代码结构

--- qconfig/config.go
+++ qconfig/config.go
@@ -1,8 +1,11 @@
 package qconfig
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/kamioair/utils/qio"
+	"github.com/spf13/viper"
+	"os"
 	"reflect"
 	"strings"
 	"unicode"
@@ -227,4 +230,63 @@ func Contains(list []string, target string) bool {
 		}
 	}
 	return false
+}
+
+// LoadConfig 统一的配置加载方法
+// filePath: 配置文件路径
+// configs: 配置映射，key为配置节名称，value为配置对象
+func LoadConfig(filePath string, configs map[string]interface{}) error {
+	// 切换到配置文件所在目录
+	configDir := ""
+	if lastSlash := strings.LastIndex(filePath, "/"); lastSlash != -1 {
+		configDir = filePath[:lastSlash]
+	} else if lastSlash := strings.LastIndex(filePath, "\\"); lastSlash != -1 {
+		configDir = filePath[:lastSlash]
+	}
+
+	if configDir != "" {
+		err := os.Chdir(configDir)
+		if err != nil {
+			return fmt.Errorf("无法切换到配置文件目录: %v", err)
+		}
+	}
+
+	// 如果配置文件不存在，则创建一个空的配置文件
+	if qio.PathExists(filePath) == false {
+		err := qio.WriteString(filePath, "", false)
+		if err != nil {
+			return fmt.Errorf("无法创建配置文件: %v", err)
+		}
+	}
+
+	// 初始化 Viper
+	viper.SetConfigFile(filePath)
+	viper.SetConfigType("yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("无法读取配置文件: %v", err)
+	}
+
+	// 从文件中读取配置到对应的对象
+	for sectionName, configObj := range configs {
+		if err := setModuleConfig(sectionName, configObj); err != nil {
+			return fmt.Errorf("加载配置节 %s 失败: %v", sectionName, err)
+		}
+	}
+
+	return nil
+}
+
+// setModuleConfig 设置配置节
+func setModuleConfig(sectionName string, configObj interface{}) error {
+	value := viper.Get(sectionName)
+	if value == nil {
+		return nil
+	}
+
+	js, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(js, configObj)
 }
