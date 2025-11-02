65116820b14d90f9b605234c634a6362e09ed983
bin <bin@qq.com>
2025-11-02 12:09:34 +0800

    继续优化配置

---
+++ qconfig/internal.go
@@ -0,0 +1,158 @@
+package qconfig
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"unicode"
+)
+
+// getBlockValues 从配置字符串中提取配置块
+func getBlockValues(input string) [][2]string {
+	// 存储提取的内容
+	var configBlocks [][2]string
+	lines := strings.Split(input, "\n")
+	for i, line := range lines {
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, "###############################") && strings.HasSuffix(line, "###############################") {
+			configBlocks = append(configBlocks, [2]string{strings.Trim(lines[i], " "), ""})
+		}
+		if len(configBlocks) > 0 {
+			configBlocks[len(configBlocks)-1][1] += line + "\n"
+		}
+	}
+
+	return configBlocks
+}
+
+// contains 判断字符串是否在列表中
+func contains(list []string, target string) bool {
+	for _, item := range list {
+		if item == target {
+			return true
+		}
+	}
+	return false
+}
+
+// toYAML 将任意对象转换为YAML格式字符串
+func toYAML(v any, indent int, excludeFields []string) string {
+	value := reflect.ValueOf(v)
+	return toYAMLValue(value, indent, excludeFields)
+}
+
+// toYAMLValue 递归处理YAML值转换
+func toYAMLValue(value reflect.Value, indent int, excludeFields []string) string {
+	switch value.Kind() {
+	case reflect.Struct:
+		return toYAMLStruct(value, indent, excludeFields)
+	case reflect.Map:
+		return toYAMLMap(value, indent, excludeFields)
+	case reflect.Slice, reflect.Array:
+		return toYAMLSlice(value, indent, excludeFields)
+	case reflect.Ptr, reflect.Interface:
+		if value.IsNil() {
+			return "null"
+		}
+		return toYAMLValue(value.Elem(), indent, excludeFields)
+	case reflect.String:
+		// 字符串类型增加双引号
+		return fmt.Sprintf("\"%v\"", value.Interface())
+	default:
+		// 基本类型直接返回值，不换行
+		return fmt.Sprintf("%v", value.Interface())
+	}
+}
+
+// toYAMLStruct 处理结构体到YAML的转换
+func toYAMLStruct(value reflect.Value, indent int, excludeFields []string) string {
+	var builder strings.Builder
+	typ := value.Type()
+	for i := 0; i < value.NumField(); i++ {
+		field := typ.Field(i)
+		// 如果字段名的首字母是小写，则跳过
+		if len(field.Name) > 0 && unicode.IsLower(rune(field.Name[0])) {
+			continue
+		}
+		// 如果字段在排除列表中，则跳过
+		if contains(excludeFields, field.Name) {
+			continue
+		}
+		fieldValue := value.Field(i)
+		// 读取字段的注释
+		comment := field.Tag.Get("comment")
+		// 如果注释存在，则在字段前面插入注释
+		if comment != "" {
+			// 将注释按换行符拆分，并在每一行前面加上#
+			commentLines := strings.Split(comment, "\n")
+			for _, line := range commentLines {
+				val := fmt.Sprintf("%s# %s\n", strings.Repeat("  ", indent), line)
+				builder.WriteString(val)
+			}
+		}
+		// 生成字段名和值
+		str1 := fmt.Sprintf("%s%s: ", strings.Repeat("  ", indent), field.Name)
+		str2 := toYAMLValue(fieldValue, indent+1, excludeFields)
+		builder.WriteString(str1)
+		if str2 != "[]" {
+			switch fieldValue.Kind() {
+			case reflect.Struct, reflect.Map, reflect.Slice, reflect.Array, reflect.Ptr, reflect.Interface:
+				builder.WriteString("\n")
+			default:
+
+			}
+		}
+		builder.WriteString(str2)
+		if i < value.NumField()-1 {
+			builder.WriteString("\n")
+		}
+	}
+	return builder.String()
+}
+
+// toYAMLMap 处理映射到YAML的转换
+func toYAMLMap(value reflect.Value, indent int, excludeFields []string) string {
+	var builder strings.Builder
+	keys := value.MapKeys()
+	for i, key := range keys {
+		mapValue := value.MapIndex(key)
+		str1 := fmt.Sprintf("%s%s: ", strings.Repeat("  ", indent), key.Interface())
+		str2 := toYAMLValue(mapValue, indent+1, excludeFields)
+		builder.WriteString(str1)
+		if str2 != "[]" {
+			switch mapValue.Kind() {
+			case reflect.Struct, reflect.Map, reflect.Slice, reflect.Array, reflect.Ptr, reflect.Interface:
+				builder.WriteString("\n")
+			default:
+
+			}
+		}
+		builder.WriteString(str2)
+		if i < len(keys)-1 {
+			builder.WriteString("\n")
+		}
+	}
+	return builder.String()
+}
+
+// toYAMLSlice 处理切片/数组到YAML的转换
+func toYAMLSlice(value reflect.Value, indent int, excludeFields []string) string {
+	var builder strings.Builder
+	count := value.Len()
+	if count == 0 {
+		builder.WriteString("[]")
+	} else {
+		for i := 0; i < value.Len(); i++ {
+			str1 := fmt.Sprintf("%s- ", strings.Repeat("  ", indent))
+			str2 := toYAMLValue(value.Index(i), indent+1, excludeFields)
+			builder.WriteString(str1)
+			builder.WriteString(str2)
+			if i < value.Len()-1 {
+				builder.WriteString("\n")
+			}
+		}
+	}
+	return builder.String()
+}
---
+++ qconfig/save.go
@@ -0,0 +1,146 @@
+package qconfig
+
+import (
+	"fmt"
+	"github.com/kamioair/utils/qio"
+	"strings"
+)
+
+// SaveConfigOptions 保存配置到文件
+type SaveConfigOptions struct {
+	SectionDescs  map[string]string // 配置节描述映射，key为配置节名称，value为描述
+	ExcludeFields []string          // 需要排除的字段列表
+}
+
+// SaveConfig 保存配置文件
+// filePath: 配置文件路径
+// configs: 配置映射，key为配置节名称，value为配置对象
+// opts: 保存选项，包含配置节描述和排除字段
+func SaveConfig(filePath string, configs map[string]interface{}, opts *SaveConfigOptions) error {
+	// 生成Base配置内容
+	configBase := map[string]any{}
+	if baseConfig, exists := configs["Base"]; exists {
+		configBase["Base"] = baseConfig
+	}
+
+	// 初始值
+	if opts == nil {
+		opts = &SaveConfigOptions{}
+	}
+	if opts.ExcludeFields == nil {
+		opts.ExcludeFields = []string{"Config"}
+	}
+
+	newCfg := ""
+	if len(configBase) > 0 {
+		newCfg += "############################### Base Config ###############################\n"
+		newCfg += "# 通用基础配置\n"
+		newCfg += toYAML(configBase, 0, opts.ExcludeFields)
+	}
+
+	// 生成模块配置内容
+	for sectionName, configObj := range configs {
+		if sectionName == "Base" {
+			continue // Base配置已经处理过了
+		}
+
+		configModule := map[string]any{}
+		configModule[sectionName] = configObj
+
+		// 获取配置节描述
+		desc := ""
+		if opts.SectionDescs != nil {
+			if descValue, exists := opts.SectionDescs[sectionName]; exists {
+				desc = descValue
+			}
+		}
+
+		mCfg := fmt.Sprintf("############################### %s Config ###############################\n", sectionName)
+		if desc != "" {
+			mCfg += fmt.Sprintf("# %s\n", desc)
+		}
+		mCfg += toYAML(configModule, 0, opts.ExcludeFields)
+
+		if !strings.HasSuffix(mCfg, fmt.Sprintf("%s: \n", sectionName)) {
+			if newCfg != "" {
+				newCfg += "\n\n"
+			}
+			newCfg += mCfg
+		}
+	}
+
+	// 尝试检测是否有变化，如果有则更新文件
+	trySave(filePath, newCfg)
+	return nil
+}
+
+// trySave 尝试保存配置文件，如果配置有变化则更新文件
+func trySave(filePath string, newCfg string) {
+	oldCfg, _ := qio.ReadAllString(filePath)
+	oldBlocks := getBlockValues(oldCfg)
+	newBlocks := getBlockValues(newCfg)
+
+	// 创建一个新的切片来存储最终的配置块
+	var finalBlocks [][2]string
+
+	// 遍历 oldBlocks，确保顺序
+	for _, ob := range oldBlocks {
+		exist := false
+		// 检查 ob 是否在 newBlocks 中
+		for _, nb := range newBlocks {
+			// 提取配置块名称进行比较（去除" Config"后缀）
+			obName := strings.TrimSpace(strings.TrimSuffix(ob[0], " Config"))
+			nbName := strings.TrimSpace(strings.TrimSuffix(nb[0], " Config"))
+			if obName == nbName {
+				exist = true
+				// 如果存在，则使用 newBlocks 中的内容
+				finalBlocks = append(finalBlocks, nb)
+				break
+			}
+		}
+		// 如果不存在，则使用 oldBlocks 中的内容
+		if !exist {
+			finalBlocks = append(finalBlocks, ob)
+		}
+	}
+
+	// 将 newBlocks 中未处理的块追加到 finalBlocks 中
+	for _, nb := range newBlocks {
+		exist := false
+		for _, fb := range finalBlocks {
+			if fb[0] == nb[0] {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			if strings.HasPrefix(nb[1], "#") &&
+				strings.Contains(nb[1], "DB Config") {
+				tlist := make([][2]string, 0)
+				for i := 0; i < len(finalBlocks); i++ {
+					if i == 1 {
+						tlist = append(tlist, nb)
+					}
+					tlist = append(tlist, finalBlocks[i])
+				}
+				finalBlocks = tlist
+			} else {
+				finalBlocks = append(finalBlocks, nb)
+			}
+		}
+	}
+
+	// 将 finalBlocks 转换为最终的配置字符串
+	finalCfg := ""
+	for _, fb := range finalBlocks {
+		finalCfg += fb[1] + "\n"
+	}
+
+	// 如果配置有变化，则保存
+	if oldCfg != finalCfg {
+		err := qio.WriteString(filePath, finalCfg, false)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
